b97a0641ec040cfea67fb5158cd904682ccc10f3
shrinidhi99 <shrinidhi99.varna@gmail.com>
2024-04-08 01:26:19 +0530

    made minor corrections to the code to make it work as desired

--- chai-aur-code/06currencyConverter/src/components/InputBox.jsx
+++ chai-aur-code/06currencyConverter/src/components/InputBox.jsx
@@ -6,7 +6,7 @@ function InputBox({
     onAmountChange,
     onCurrencyChange,
     currencyOptions = [],
-    selectCurrency = "usd",
+    selectCurrency,
     amountDisable = false,
     currencyDisable = false,
     className = "",
